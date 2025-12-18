24413d03cd807c3d23331aae45e18e5240478200
duke <zzjnp112@gmail.com>
2025-12-18 16:22:09 +0800

    first

--- simulation_fly/src/camer_bridge.cpp
+++ simulation_fly/src/camer_bridge.cpp
@@ -2,18 +2,25 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include <gz/transport/Node.hh>
 #include <gz/msgs/image.pb.h>
 #include <gz/msgs/imu.pb.h>
+#include <gz/msgs/laserscan.pb.h>
+#include <gz/msgs/pointcloud_packed.pb.h>
 #include <mutex>
 #include <cstring>
 #include <csignal>
+#include <rosgraph_msgs/msg/clock.hpp>
+#include <gz/msgs/clock.pb.h>
+#include <memory>
 #include <optional>
 
-class GzToRos2ImageBridge : public rclcpp::Node
+class GzToRos2Bridge : public rclcpp::Node
 {
 public:
-    GzToRos2ImageBridge()
+    GzToRos2Bridge()
         : Node("gz_to_agiros_bridge")
     {
         // ---------------- 参数 ----------------
@@ -25,6 +32,11 @@ public:
         this->declare_parameter<std::string>("depth_frame_id", "camera_link");
         this->declare_parameter<std::string>("imu_topic", "/imu/data");
         this->declare_parameter<std::string>("imu_frame_id", "camera_link");
+        this->declare_parameter<std::string>("gz_lidar_scan_topic", "/lidar");
+        this->declare_parameter<std::string>("agiros_lidar_scan_topic", "/drone_0_/lidar/scan");
+        this->declare_parameter<std::string>("gz_lidar_points_topic", "/lidar/points");
+        this->declare_parameter<std::string>("agiros_lidar_points_topic", "/drone_0_/lidar/points");
+        this->declare_parameter<std::string>("lidar_frame_id", "world");
 
         gz_rgb_topic_  = this->get_parameter("gz_rgb_topic").as_string();
         ros_rgb_topic_ = this->get_parameter("agiros_rgb_topic").as_string();
@@ -34,13 +46,19 @@ public:
         depth_frame_id_ = this->get_parameter("depth_frame_id").as_string();
         imu_topic_ = this->get_parameter("imu_topic").as_string();
         imu_frame_id_ = this->get_parameter("imu_frame_id").as_string();
+        gz_lidar_scan_topic_ = this->get_parameter("gz_lidar_scan_topic").as_string();
+        ros_lidar_scan_topic_ = this->get_parameter("agiros_lidar_scan_topic").as_string();
+        gz_lidar_points_topic_ = this->get_parameter("gz_lidar_points_topic").as_string();
+        ros_lidar_points_topic_ = this->get_parameter("agiros_lidar_points_topic").as_string();
+        lidar_frame_id_ = this->get_parameter("lidar_frame_id").as_string();
 
         // ---------------- 发布器 ----------------
-    
-        
         rgb_pub_ = this->create_publisher<sensor_msgs::msg::Image>(ros_rgb_topic_, 100);
         depth_pub_ = this->create_publisher<sensor_msgs::msg::Image>(ros_depth_topic_, 100);
         imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(imu_topic_, 100);
+        lidar_scan_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>(ros_lidar_scan_topic_, 100);
+        lidar_points_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(ros_lidar_points_topic_, 100);
+        clock_pub_ = this->create_publisher<rosgraph_msgs::msg::Clock>("/clock", 10);
 
         // ---------------- CameraInfo 发布器 ----------------
         auto info_qos = rclcpp::QoS(rclcpp::KeepLast(10));
@@ -53,25 +71,48 @@ public:
         initializeCameraInfo();
 
         // ---------------- Gazebo 订阅 ----------------
-        if (!gz_node_.Subscribe(gz_rgb_topic_, &GzToRos2ImageBridge::rgbCallback, this))
+        // RGB
+        if (!gz_node_.Subscribe(gz_rgb_topic_, &GzToRos2Bridge::rgbCallback, this))
             RCLCPP_ERROR(this->get_logger(), "订阅 RGB 失败: %s", gz_rgb_topic_.c_str());
         else
             RCLCPP_INFO(this->get_logger(), "成功订阅 RGB: %s", gz_rgb_topic_.c_str());
 
-        if (!gz_node_.Subscribe(gz_depth_topic_, &GzToRos2ImageBridge::depthCallback, this))
+        //time
+        if(!gz_node_.Subscribe("/clock", &GzToRos2Bridge::gzClockCallback, this))
+            RCLCPP_ERROR(this->get_logger(), "订阅 Gazebo Clock 失败: /clock");
+        else
+            RCLCPP_INFO(this->get_logger(), "成功订阅 Gazebo Clock: /clock");
+
+        // Depth
+        if (!gz_node_.Subscribe(gz_depth_topic_, &GzToRos2Bridge::depthCallback, this))
             RCLCPP_ERROR(this->get_logger(), "订阅 Depth 失败: %s", gz_depth_topic_.c_str());
         else
             RCLCPP_INFO(this->get_logger(), "成功订阅 Depth: %s", gz_depth_topic_.c_str());
 
-        if (!gz_node_.Subscribe("/imu", &GzToRos2ImageBridge::gzImuCallback, this))
+        // IMU
+        if (!gz_node_.Subscribe("/imu", &GzToRos2Bridge::gzImuCallback, this))
             RCLCPP_ERROR(this->get_logger(), "订阅 Gazebo IMU 失败: /imu");
         else
             RCLCPP_INFO(this->get_logger(), "成功订阅 Gazebo IMU: /imu");
+
+        // 激光雷达扫描数据
+        if (!gz_node_.Subscribe(gz_lidar_scan_topic_, &GzToRos2Bridge::lidarScanCallback, this))
+            RCLCPP_ERROR(this->get_logger(), "订阅激光雷达扫描失败: %s", gz_lidar_scan_topic_.c_str());
+        else
+            RCLCPP_INFO(this->get_logger(), "成功订阅激光雷达扫描: %s", gz_lidar_scan_topic_.c_str());
+
+        // 激光雷达点云数据
+        if (!gz_node_.Subscribe(gz_lidar_points_topic_, &GzToRos2Bridge::lidarPointsCallback, this))
+            RCLCPP_ERROR(this->get_logger(), "订阅激光雷达点云失败: %s", gz_lidar_points_topic_.c_str());
+        else
+            RCLCPP_INFO(this->get_logger(), "成功订阅激光雷达点云: %s", gz_lidar_points_topic_.c_str());
 
         RCLCPP_INFO(this->get_logger(), "Gazebo → ROS2 桥启动");
         RCLCPP_INFO(this->get_logger(), "RGB 发布到: %s", ros_rgb_topic_.c_str());
         RCLCPP_INFO(this->get_logger(), "Depth 发布到: %s", ros_depth_topic_.c_str());
         RCLCPP_INFO(this->get_logger(), "IMU 发布到: %s", imu_topic_.c_str());
+        RCLCPP_INFO(this->get_logger(), "Lidar Scan 发布到: %s", ros_lidar_scan_topic_.c_str());
+        RCLCPP_INFO(this->get_logger(), "Lidar Points 发布到: %s", ros_lidar_points_topic_.c_str());
 
         std::signal(SIGINT, [](int){
             rclcpp::shutdown();
@@ -125,6 +166,16 @@ private:
         depth_info_pub_->publish(depth_camera_info_);
     }
 
+     void gzClockCallback(const gz::msgs::Clock &_msg)
+    {
+        rosgraph_msgs::msg::Clock ros_clock_msg;
+        ros_clock_msg.clock.sec = _msg.sim().sec();
+        ros_clock_msg.clock.nanosec = _msg.sim().nsec();
+
+        // 发布到 ROS2
+        clock_pub_->publish(ros_clock_msg);
+    }
+
     // ---------------- Gazebo IMU 回调 ----------------
     void gzImuCallback(const gz::msgs::IMU &gz_msg)
     {
@@ -160,6 +211,112 @@ private:
         }
 
         imu_pub_->publish(ros_msg);
+    }
+
+    // ---------------- 激光雷达扫描数据回调 ----------------
+    void lidarScanCallback(const gz::msgs::LaserScan &gz_msg)
+    {
+        sensor_msgs::msg::LaserScan ros_msg;
+
+        // 时间戳
+        auto s = gz_msg.header().stamp();
+        ros_msg.header.stamp.sec = s.sec();
+        ros_msg.header.stamp.nanosec = s.nsec();
+        ros_msg.header.frame_id = lidar_frame_id_;
+
+        // 设置雷达参数
+        ros_msg.angle_min = gz_msg.angle_min();
+        ros_msg.angle_max = gz_msg.angle_max();
+        ros_msg.angle_increment = gz_msg.angle_step();
+        ros_msg.time_increment = 0.0;  // 固定扫描，没有时间增量
+        ros_msg.scan_time = 0.0;       // 固定扫描，没有扫描时间
+        ros_msg.range_min = gz_msg.range_min();
+        ros_msg.range_max = gz_msg.range_max();
+
+        // 复制距离数据
+        ros_msg.ranges.resize(gz_msg.ranges_size());
+        for (int i = 0; i < gz_msg.ranges_size(); ++i) {
+            ros_msg.ranges[i] = gz_msg.ranges(i);
+        }
+
+        // 复制强度数据（如果存在）
+        if (gz_msg.intensities_size() > 0) {
+            ros_msg.intensities.resize(gz_msg.intensities_size());
+            for (int i = 0; i < gz_msg.intensities_size(); ++i) {
+                ros_msg.intensities[i] = gz_msg.intensities(i);
+            }
+        } else {
+            ros_msg.intensities.clear();
+        }
+
+        lidar_scan_pub_->publish(ros_msg);
+    }
+
+    // ---------------- 激光雷达点云数据回调 ----------------
+    void lidarPointsCallback(const gz::msgs::PointCloudPacked &gz_msg)
+    {
+        sensor_msgs::msg::PointCloud2 ros_msg;
+
+        // 时间戳
+        auto s = gz_msg.header().stamp();
+        ros_msg.header.stamp.sec = s.sec();
+        ros_msg.header.stamp.nanosec = s.nsec();
+        ros_msg.header.frame_id = lidar_frame_id_;
+
+        // 设置点云参数
+        ros_msg.height = 1;  // 非组织化点云
+        ros_msg.width = gz_msg.data().size() / gz_msg.point_step();
+        ros_msg.is_bigendian = false;
+        ros_msg.point_step = gz_msg.point_step();
+        ros_msg.row_step = ros_msg.width * ros_msg.point_step;
+        ros_msg.is_dense = true;
+
+        // 设置字段
+        ros_msg.fields.clear();
+        for (const auto &field : gz_msg.field()) {
+            sensor_msgs::msg::PointField pf;
+            pf.name = field.name();
+            pf.offset = field.offset();
+            
+            // 转换数据类型
+            switch (field.datatype()) {
+                case gz::msgs::PointCloudPacked::Field::INT8:
+                    pf.datatype = sensor_msgs::msg::PointField::INT8;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::UINT8:
+                    pf.datatype = sensor_msgs::msg::PointField::UINT8;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::INT16:
+                    pf.datatype = sensor_msgs::msg::PointField::INT16;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::UINT16:
+                    pf.datatype = sensor_msgs::msg::PointField::UINT16;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::INT32:
+                    pf.datatype = sensor_msgs::msg::PointField::INT32;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::UINT32:
+                    pf.datatype = sensor_msgs::msg::PointField::UINT32;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::FLOAT32:
+                    pf.datatype = sensor_msgs::msg::PointField::FLOAT32;
+                    break;
+                case gz::msgs::PointCloudPacked::Field::FLOAT64:
+                    pf.datatype = sensor_msgs::msg::PointField::FLOAT64;
+                    break;
+                default:
+                    pf.datatype = sensor_msgs::msg::PointField::FLOAT32;
+            }
+            
+            pf.count = 1;
+            ros_msg.fields.push_back(pf);
+        }
+
+        // 复制数据
+        ros_msg.data.resize(gz_msg.data().size());
+        std::memcpy(ros_msg.data.data(), gz_msg.data().data(), gz_msg.data().size());
+
+        lidar_points_pub_->publish(ros_msg);
     }
 
     // ---------------- Gazebo Image → ROS Image ----------------
@@ -208,13 +365,20 @@ private:
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr rgb_pub_, depth_pub_;
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
     
+    // 雷达发布器
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr lidar_scan_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_points_pub_;
+    
     // CameraInfo发布器
     rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr rgb_info_pub_, depth_info_pub_;
-
+    rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_pub_;
     std::string gz_rgb_topic_, ros_rgb_topic_;
     std::string gz_depth_topic_, ros_depth_topic_;
     std::string rgb_frame_id_, depth_frame_id_;
     std::string imu_topic_, imu_frame_id_;
+    std::string gz_lidar_scan_topic_, ros_lidar_scan_topic_;
+    std::string gz_lidar_points_topic_, ros_lidar_points_topic_;
+    std::string lidar_frame_id_;
 
     // CameraInfo消息
     sensor_msgs::msg::CameraInfo rgb_camera_info_, depth_camera_info_;
@@ -223,7 +387,7 @@ private:
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<GzToRos2ImageBridge>();
+    auto node = std::make_shared<GzToRos2Bridge>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
